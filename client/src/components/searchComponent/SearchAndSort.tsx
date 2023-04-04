2c7e13c4b62c7a871d3061d56b5cd64ba6c45ce2
Freeze35 <72322938+Freeze35@users.noreply.github.com>
2023-04-04 16:49:06 +0800

    Revert "Test ssl"

--- client/src/components/searchComponent/SearchAndSort.tsx
+++ client/src/components/searchComponent/SearchAndSort.tsx
@@ -1,42 +1,34 @@
-import React, {useCallback, useContext} from 'react';
+import React, {useContext} from 'react';
 import "./SearchAndSort.css"
 import {observer} from "mobx-react-lite";
 import {Row} from "react-bootstrap";
+import {Context} from "../../index";
 import {fetchBooksFirstTime} from "../../api/fetchingApi";
 import SortComponent from "../sortComponent/SortComponent";
+
 import {useNavigate} from "react-router-dom";
 import {ReactComponent as SearchLoop} from '../../assets/images/magnifying-glass-solid.svg';
-import {Context} from "../../ContextProvider";
 
-
-
-const SearchAndSort = observer(() => {
+const SearchAndSort:React.FC = observer(() => {
 
     const {booksStore} = useContext(Context)
     const navigate = useNavigate()
-    let bookSearchQuery = booksStore.searchQuery
 
-    const SetDataButton = useCallback((bookSearchQuery:string) => {
-        console.log({bookSearchQuery2: bookSearchQuery})
-        if (bookSearchQuery) {
-            navigate(`/booklist`)
-            fetchBooksFirstTime(bookSearchQuery, booksStore,
-                booksStore.optionRelevance,booksStore.subjectCategory).then()
-        }
-    },[])
-
-
-    const setDataEnter = useCallback((event: React.KeyboardEvent<HTMLInputElement>) => {
-        console.log("SetDataEnter")
-        if (event.key === "Enter" && bookSearchQuery) {
+    const SetDataButton = () => {
+        if (booksStore.searchQuery) {
             navigate(`/booklist`)
             fetchBooksFirstTime(booksStore.searchQuery, booksStore,
                 booksStore.optionRelevance,booksStore.subjectCategory).then()
         }
-    },[])
+    }
 
-    const setSearchQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
-             booksStore.setSearchQuery(e.target.value)
+
+    const SetDataEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter" && booksStore.searchQuery) {
+            navigate(`/booklist`)
+            fetchBooksFirstTime(booksStore.searchQuery, booksStore,
+                booksStore.optionRelevance,booksStore.subjectCategory).then()
+        }
     }
 
     return (
@@ -45,13 +37,13 @@ const SearchAndSort = observer(() => {
             <Row id="row_search" className="position_set">
                 <input id="input_search" className="position_set input_search"
                        placeholder={`Поиск..`}
-                       value={bookSearchQuery}
-                       onChange={setSearchQuery}
+                       value={booksStore.searchQuery}
+                       onChange={(e: React.ChangeEvent<HTMLInputElement>) => booksStore.setSearchQuery(e.target.value)}
                        type="text"
-                       onKeyDown={setDataEnter}
-                />
+                       onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => SetDataEnter(e)}
+                ></input>
                 <button id="button_search" aria-label="Поиск..." className="position_set button_search"
-                        onClick={()=>SetDataButton(bookSearchQuery)}>
+                        onClick={() => SetDataButton()}>
                     <SearchLoop id="search_loop" className="search_loop"/></button>
             </Row>
             <SortComponent/>
--- client/src/declare.d.ts
+++ client/src/declare.d.ts
@@ -1,8 +1,4 @@
 declare module "*.png";
-declare module "*.svg"{
-    const content: any;
-    export const ReactComponent: any;
-    export default content;
-}
+declare module "*.svg";
 declare module "*.jpeg";
 declare module "*.jpg";
